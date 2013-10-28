522688fa241292e20e3191da845ae2e3f56b8e93
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2013-10-28 20:06:11 -0200

    Inicial

---
+++ ext/ffmpeg-ruby/ffmpeg_ruby_audio.h
@@ -0,0 +1,9 @@
+#ifndef	__FFMPEG_RUBY_AUDIO_H__
+#define	__FFMPEG_RUBY_AUDIO_H__
+
+#include "ffmpeg_ruby.h"
+
+// Object Lifetime
+VALUE reader_register_class(VALUE module, VALUE super);
+
+#endif // __FFMPEG_RUBY_AUDIO_H__
